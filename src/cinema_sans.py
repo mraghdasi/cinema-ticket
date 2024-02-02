09f0ba5b896890d3f708f083a72c63e0dadaa91c
mohamadrexa <yazdanifar@hotmail.com>
2024-02-01 15:49:49 -1000

    ticket.py and cinema_sans.py

---
+++ src/cinema_sans.py
@@ -0,0 +1,56 @@
+from src.db.db_operations import DBOperation
+from datetime import datetime
+
+class Cinema_sans(DBOperation):
+    """
+            Class To Make Cinema_sans Instances.
+    """
+    start_time: datetime.time
+    end_time: datetime.time
+    film_id: int
+    hall_id: int
+    price: int
+
+    def __init__(self, start_time, end_time, film_id, hall_id, price):
+        """
+                Initialize Instance (Constructor Method)
+        """
+        self.start_time = start_time
+        self.end_time = end_time
+        self.film_id = film_id
+        self.hall_id = hall_id
+        self.price = price
+
+    def __str__(self):
+        return (f'Start Time:{self.start_time} | End Time:{self.end_time} |'
+                f' Film ID:{self.film_id} | Hall ID:{self.hall_id} | Price:{self.price}')
+
+
+    def create(**kwargs):
+        """
+            Create New Row Of Cinema sans in cinema_sans Table in Database
+
+        """
+        super().create('cinema_sans', kwargs.get(
+            'columns', None), kwargs.get('values', None))
+    def read(**kwargs):
+        """
+        Get An Existing cinema sans From cinema_sans Table in Database
+        """
+        super().read(kwargs.get('columns', None), 'cinema_sans', kwargs.get(
+            'condition', None), kwargs.get('order', None))
+
+    def update(**kwargs):
+        """
+        Update An Existing Cinema Sans In cinema_sans Table in Database
+
+        """
+        super().update('cinema_sans', kwargs.get(
+            'columns', None), kwargs.get('condition', None))
+
+    def delete(**kwargs):
+        """
+        Delete An Existing cinema sans From cinema_sans Table in Database
+
+        """
+        super().delete('cinema_sans', kwargs.get('condition', None))
---
+++ src/ticket.py
@@ -0,0 +1,52 @@
+from src.db.db_operations import DBOperation
+
+class Ticket(DBOperation):
+    """
+            Class To Make Subscription Instances.
+    """
+    cinema_sans_id: int
+    user_id: int
+    sit_number: int
+
+    def __init__(self, cinema_sans_id, user_id, sit_number):
+        """
+        Initialize Instance (Constructor Method)
+        """
+        self.cinema_sans_id = cinema_sans_id
+        self.user_id = user_id
+        self.sit_number = sit_number
+
+    def __str__(self):
+        return f': Cinema Sans ID: {self.cinema_sans_id} | User ID: {self.user_id} | Sit Number: {self.sit_number}'
+
+    def create(**kwargs):
+        """
+        Create New Row Of Ticket in ticket Table in Database
+        """
+        super().create('ticket', kwargs.get(
+            'columns', None), kwargs.get('values', None))
+
+    def read(**kwargs):
+        """
+        Get An Existing ticket From ticket Table in Database
+
+        """
+        super().read(kwargs.get('columns', None), 'ticket', kwargs.get(
+            'condition', None), kwargs.get('order', None))
+
+
+    def update(**kwargs):
+        """
+        Update An Existing ticket In ticket Table in Database
+
+        """
+        super().update('ticket', kwargs.get(
+            'columns', None), kwargs.get('condition', None))
+
+ def delete(**kwargs):
+        """
+        Delete An Existing ticket  From ticket Table in Database
+
+        """
+        super().delete('ticket', kwargs.get('condition', None))
+
