042f36f5ad4625143538eab1d2e655561b2b704a
soroush moradpour <46293975+soroushmp@users.noreply.github.com>
2024-02-02 04:35:23 +0330

    1. Add Staticmethod to CRUD methods in subscription.py,user.py 2. Add film.py 3. Add Film Class 4. Add CRUD Methods to Film Class 5. Rename validators.py to custom_validators.py 6. Add MinAgeNotPositive Class To custom_exceptions.py 7. Add min_age_validator method to custom_validators.py 8. Rename UserValidator to Validator in custom_validators.py 9. Rename All UserValidator (Usages) to Validator in user.py (All Project) 10. Add film_rate.py 11. Add FilmRate Class 12. Add CRUD Methods to FilmRate Class 13. Change Color of Text in User Box in cinema-ticket.drawio 14. Add rate_validator Method to custom_validators.py 15. Add RateNotBetweenZeroAndFive Class To custom_exceptions.py

--- src/film.py
+++ src/film.py
@@ -1,70 +1,72 @@
-from datetime import datetime
-
 from src.db.db_operations import DBOperation
+from src.utils.custom_validators import Validator
 
 
-class Subscription(DBOperation):
+class Film(DBOperation):
     """
-        Class To Make Subscription Instances. 
+        Class To Make Film Instances. 
     """
-    user_id: int
-    package_id: int
-    expire_at: datetime
+    title: str
+    min_age: int
 
-    def __init__(self, user_id, package_id, expire_at):
+    def __init__(self, title, min_age):
         """
         Initialize Instance (Constructor Method)
         """
-        self.user_id = user_id
-        self.package_id = package_id
-        self.expire_at = expire_at
+        self.title = title
+
+        validate_min_age = Validator.validate(min_age, (Validator.min_age_validator,))
+        if isinstance(validate_min_age, bool):
+            self.min_age = validate_min_age
+        else:
+            raise Exception(validate_min_age)
 
     def __str__(self):
-        return f'User Id: {self.user_id} | Package Id: {self.package_id} | Expire At: {self.expire_at}'
+        return f'Title : {self.title} | Min Age: {self.min_age}'
 
     @staticmethod
     def create(**kwargs):
         """
-        Create New Row Of Subscription in Subscription Table in Database
+        Create New Row Of Film in Film Table in Database
         :param kwargs:
             columns: string of columns names, comma separated (col1, col2, col3)
             values: string of columns values, comma separated (val1, val2, val3)
         :return:
         """
-        super().create('subscription', kwargs.get(
+        super().create('film', kwargs.get(
             'columns', None), kwargs.get('values', None))
 
     @staticmethod
     def read(**kwargs):
         """
-        Get An Existing Subscription From Subscription Table in Database
+        Get An Existing Film From Film Table in Database
         :param kwargs:
             columns: string of columns names, comma separated (col1, col2, col3)
             condition: string of conditions (col1 = 'val1'), Default Value None
             order: tuple of two value (col_name, ASC|DESC) (col1, ASC), Default Value None
         :return:
         """
-        super().read(kwargs.get('columns', None), 'subscription', kwargs.get(
+        super().read(kwargs.get('columns', None), 'film', kwargs.get(
             'condition', None), kwargs.get('order', None))
 
     @staticmethod
     def update(**kwargs):
         """
-        Update An Existing Subscription In Subscription Table in Database
+        Update An Existing Film In Film Table in Database
         :param kwargs:
             columns: string of columns names and values, comma separated "col1 = val1, col2 = val2, col3 = val3"
             condition: string of conditions (col1 = 'val1'), Default Value None
         :return:
         """
-        super().update('subscription', kwargs.get(
+        super().update('film', kwargs.get(
             'columns', None), kwargs.get('condition', None))
 
     @staticmethod
     def delete(**kwargs):
         """
-        Delete An Existing Subscription From Subscription Table in Database
+        Delete An Existing Film From Film Table in Database
         :param kwargs:
             condition: string of conditions (col1 = 'val1'), Default Value None
         :return:
         """
-        super().delete('subscription', kwargs.get('condition', None))
+        super().delete('film', kwargs.get('condition', None))
