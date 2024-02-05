3f2d5a52ee8d8c2e588b8be30f1f037c0bec607a
mohamadrexa <yazdanifar@hotmail.com>
2024-02-04 23:45:12 -1000

    buy-Ticket and Card_management

--- src/client/service/buy_ticket.py
+++ src/client/service/buy_ticket.py
@@ -24,3 +24,39 @@ def main(user_info,movie=None):
         #             payment stuff  and changes in db (should use othere modules and db)
         # 
         # returns user_info
+
+
+
+
+
+        #======================Please Check my Code =========================
+        movie_list = [('spiderman', 'sat', '2 to 4 (price)'), ('spiderman', 'sat', '5 to 7 (price)'),
+                      ('spiderman', 'sun', '4 to 6 (price)'), ('batman', 'sat', '3 to 5 (price)'),
+                      ('batman', 'sat', '6 to 8 (price)'), ('batman', 'sun', '5 to 7 (price)'),
+                      ('joker', 'sat', '4 to 6 (price)'), ('joker', 'sat', '7 to 9 (price)'),
+                      ('joker', 'sun', '6 to 8 (price)')]
+
+        movies_dict = {}
+
+        for m, d, t in movie_list:
+            if m not in movies_dict:
+                movies_dict[m] = {'sat': [], 'sun': []}
+            movies_dict[m][d].append(t)
+
+        movie_name = input("Enter movie name: ")
+        if movie_name in movies_dict:
+            showtimes = movies_dict[movie_name]
+            print("{0}:".format(movie_name))
+            for day_number, (day, times) in enumerate(showtimes.items(), 1):
+                print("{0}. {1}:".format(day_number, day))
+                for i, t in enumerate(times):
+                    print("  {0}. {1}".format(i + 1, t))
+
+            selected_time = input("Enter day and time number: ")
+            day_number, time_number = map(int, selected_time.split('-'))
+            day = list(showtimes.keys())[day_number - 1]
+            selected_time = showtimes[day][time_number - 1]
+            print("Selected time: {0} - {1}".format(day, selected_time))
+            print("***At this step the user should be taken to the payment page***")
+        else:
+            print("Movie not found.")
--- src/client/service/card_management.py
+++ src/client/service/card_management.py
@@ -12,4 +12,46 @@
 #             input(management options)
         
 #         return user_info
-        
+
+
+
+
+#=========================***Plese check My Code:***===============MRY=================
+import sys
+
+card_list = ['2656435571048868', '7988634497364006', '4328353309086601', '2498525345064556', '3500037762653692', '3971687107588513', '8353272705341641', '7215425071807050', '6715821371280480', '7644452566589771']
+if not card_list:
+    print("There is no card in your profile.")
+else:
+    print("Your cards:")
+    for i, card in enumerate(card_list, start=1):
+        print(f"{i}.{card}")
+    selected = input("Please choose a card number: ")
+    try:
+        index = int(selected) - 1
+        selected_card = card_list[index]
+        print("Selected card: ", selected_card)
+        while True:
+            print("You can choose one of the following options:")
+            print("1. Edit card number")
+            print("2. Change CVV2")
+            print("3. Change expiration date")
+            print("4. Cancel")
+            choice = input("Please choose an option: ")
+            if choice == '1':
+                print("Going to update.py...")
+                sys.exit()
+            elif choice == '2':
+                print("Going to update.py...")
+                sys.exit()
+            elif choice == '3':
+                print("Going to update.py...")
+                sys.exit()
+            elif choice == '4':
+                print("Canceled.")
+                break
+            else:
+                print("Invalid selection. Please try again.")
+    except (ValueError, IndexError):
+        print("Invalid selection.")
+
