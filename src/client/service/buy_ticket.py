9d3c109170c4a9de4e6f6005c98a48f4101580f3
mohamadrexa <yazdanifar@hotmail.com>
2024-02-05 08:21:32 -1000

    edit buy_ticket and buy_subscriptions

--- src/client/service/buy_ticket.py
+++ src/client/service/buy_ticket.py
@@ -2,61 +2,38 @@ import os
 
 
 def main(user_info, movie=None):
+    # Create dictionary to map day codes to numeric values
+    day_codes = {'Sat': 1, 'Sun': 2, 'Mon': 3, 'Tue': 4, 'Wed': 5, 'Thu': 6, 'Fri': 7}
     while True:
-        print("please choose yor movie")
+        print("Please choose your movie")
 
-        # {'spiderman': {'sat' : ['2 to 4 (price)' , '5 to 7 (price)'] , 'sun' : ['4 to 6 (price)']} ,
-        # 'batman' : {...} , 'joker' : {...} , ...}
-        # available_movies = "this should be read from somewhere"
-        # user_input = input(f"Here should be a list of <available_movies.keys>")
-
-        # if user_input == one of the movies :
-        #     print("choose the session and day"):
-        #         print(available_movies[one of the movies] as key values)
-        #               1. sat : 1.2 to 4 (price)
-        #                        2.5 to 7 (price)
-        #               2. sun : 1.4 to 6 (price)
-        #               3. ...
-        #               4. ...
-        # 
-        #         user_input = input("a list of available sessions for the movie")
-        #                      input should be like 1-2 (sat 5 to 7)
-        # 
-        #         if user_input == one of the sessions:
-        #             payment stuff  and changes in db (should use other modules and db)
-        # 
-        # returns user_info
-
-        # ======================Please Check my Code =========================
-
-        # near perfect execution it misses a few key stuff: (check below)
-
+        # Available movies
         movies = [
             {
-                'name': 'spiderman',
+                'name': 'Spiderman',
                 'min_age': 12,
                 'rating': 8.8,
                 'length': 148,
                 'sans': [
-                    {'day': 'sat', 'start_time': '14:00', 'end_time': '16:00'},
-                    {'day': 'sat', 'start_time': '18:00', 'end_time': '20:00'},
-                    {'day': 'Wednesday', 'start_time': '21:00', 'end_time': '23:00'},
-                    {'day': 'Wednesday', 'start_time': '19:00', 'end_time': '20:00'},
-                    {'day': 'Thursday', 'start_time': '14:00', 'end_time': '16:00'},
-                    {'day': 'Thursday', 'start_time': '21:00', 'end_time': '23:00'}
+                    {'day': 'Sat', 'start_time': '14:00', 'end_time': '16:00'},
+                    {'day': 'Sat', 'start_time': '18:00', 'end_time': '20:00'},
+                    {'day': 'Wed', 'start_time': '21:00', 'end_time': '23:00'},
+                    {'day': 'Wed', 'start_time': '19:00', 'end_time': '20:00'},
+                    {'day': 'Thu', 'start_time': '14:00', 'end_time': '16:00'},
+                    {'day': 'Thu', 'start_time': '21:00', 'end_time': '23:00'}
                 ],
                 'comments number': 1050,
                 'ticket price': 12
             },
             {
-                'name': 'batman',
+                'name': 'Batman',
                 'min_age': 13,
                 'rating': 9.0,
                 'length': 152,
                 'sans': [
-                    {'day': 'Friday', 'start_time': '15:00', 'end_time': '17:00'},
-                    {'day': 'Saturday', 'start_time': '12:00', 'end_time': '14:00'},
-                    {'day': 'Sunday', 'start_time': '18:00', 'end_time': '20:00'}
+                    {'day': 'Fri', 'start_time': '15:00', 'end_time': '17:00'},
+                    {'day': 'Sat', 'start_time': '12:00', 'end_time': '14:00'},
+                    {'day': 'Sun', 'start_time': '18:00', 'end_time': '20:00'}
                 ],
                 'comments number': 876,
                 'ticket price': 11
@@ -69,42 +46,98 @@ def main(user_info, movie=None):
             name = movie['name']
             sans = movie['sans']
             if name not in movies_dict:
-                movies_dict[name] = {'sat': [], 'sun': [], 'mon': [], 'tue': [], 'wed': [], 'thu': [], 'fri': []}
+                movies_dict[name] = {'Sat': [], 'Sun': [], 'Mon': [], 'Tue': [], 'Wed': [], 'Thu': [], 'Fri': []}
             for san in sans:
-                day = san['day'].lower()[:3]
+                day = san['day'][:3]
                 start_time = san['start_time']
                 end_time = san['end_time']
-                time_range = f"{start_time} to {end_time} (price {movie['ticket price']})"
-                movies_dict[name][day].append(time_range)
+                time_range = f"{start_time} to {end_time} (price {movie['ticket price']} USD)"
+                movies_dict[name][day] = movies_dict[name].get(day, []) + [time_range]
 
+        # Print available movies
         print("List of available movies:")
-        i = 1
-        for movie in movies:
-            print(f"{i}.{movie['name']}")
-            i += 1
+        for i, movie in enumerate(movies, start=1):
+            print(f"{i}. {movie['name']}")
+        print("q. Quit")
 
+        # Get user input for movie choice
+        movie_choice = input("Enter movie name or number: ").strip().lower()
         # ----------------
 
         # here user should be able to type the movie name or the movie number on menu 1.spiderman
-        # the correct input is either 1 or spiderman
+        # the correct input is either 1 or spiderman                            *
         # and you should use .strip() and .lower() on this types of inputs
 
-        movie_name = input("Enter movie name: ")
+        # ------------------
 
-        # --------------
+        if movie_choice == 'q':
+            print('Exiting the program...')
+            break
 
-        if movie_name in movies_dict:
-            showtimes = movies_dict[movie_name]
-            print("{0}:".format(movie_name))
-            days_with_showtimes = []
-            for day, times in showtimes.items():
-                if times:
-                    days_with_showtimes.append(day)
-            for day_number, day in enumerate(days_with_showtimes, 1):
-                print("{0}. {1}:".format(day_number, day))
-                times = showtimes[day]
-                for i, t in enumerate(times):
-                    print("  {0}. {1}".format(i + 1, t))
+        if movie_choice not in [str(i) for i in range(1, len(movies) + 1)] + [movie['name'].lower() for movie in movies]:
+            print("Invalid movie. Please try again.")
+            continue
+
+        if movie_choice.isdigit():
+            movie = movies[int(movie_choice) - 1]
+        else:
+            for m in movies:
+                if m['name'].lower() == movie_choice:
+                    movie = m
+                    break
+
+        showtimes = movies_dict[movie['name']]
+
+        # Print available showtimes
+        print("\n{0}:".format(movie['name']))
+
+        for day, times in showtimes.items():
+            if not times:
+                continue
+            numerized_day = day_codes[day]
+            print("{0}. {1}:".format(numerized_day, day))
+            for i, t in enumerate(times, start=1):
+                print("  {0}. {1}".format(i, t))
+
+        while True:
+            # Get user input for showtime choice
+            selected_time = input("Enter day and time number (e.g. 1-2): ").strip()
+
+            if selected_time == 'q':
+                break
+
+            # Validate user input for showtime choice
+            try:
+                day_number, time_number = map(int, selected_time.split('-'))
+            except ValueError:
+                print("Invalid input. Please enter day and time number like '1-2'.")
+                continue
+
+            if not 1 <= day_number <= len(showtimes) or not 1 <= time_number <= len(
+                    showtimes[list(showtimes.keys())[day_number - 1]]):
+                print("Invalid input. Please enter a valid day and time number.")
+                continue
+
+            day = list(showtimes.keys())[day_number - 1]
+            selected_time = showtimes[day][time_number - 1]
+
+            print("Selected time: {0} - {1}".format(day, selected_time))
+
+            # Print payment information
+            print("\n============================")
+            print("Movie Name: {0}".format(movie['name']))
+            print("{0} {1}".format(day, selected_time))
+            print("Price: {0} USD".format(movie['ticket price']))
+            print("\n1. Confirm Payment")
+            print("2. Go back to movie selection")
+            print("q. Quit")
+
+            # Get user input for payment choice
+            payment_choice = input("Enter your choice (1, 2, or q): ").strip()
+
+            if payment_choice == '1':
+                print("Payment successful. Thank you for your purchase!")
+                return user_info
             # -----------
 
             # you didn't check for correct input here
@@ -114,17 +147,12 @@ def main(user_info, movie=None):
             # opt1 : use try except
             # opt2 : use if elif else
 
-            selected_time = input("Enter day and time number: ")
+            elif payment_choice == '2':
+                break
 
-            # -----------
-            day_number, time_number = map(int, selected_time.split('-'))
-            day = days_with_showtimes[day_number - 1]
-            selected_time = showtimes[day][time_number - 1]
-            print("Selected time: {0} - {1}".format(day, selected_time))
-            print("***At this step the user should be taken to the payment page***")
-        else:
-            print("Movie not found.")
-
+            elif payment_choice == 'q':
+                print('Exiting the program...')
+                return user_info
 
 # -------------------
 
@@ -147,6 +175,7 @@ def main(user_info, movie=None):
 # 4. pay attention that if everything goes ok the program should stop working
 
 # -------------------
+
 
 
 main('m', 'm')
