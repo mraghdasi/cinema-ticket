9d93d010fe7efd5e66538e54c6a2f9ac1db729a5
mohamadrexa <yazdanifar@hotmail.com>
2024-02-05 00:19:30 -1000

    buy_subscription.py

--- src/client/service/buy_subscription.py
+++ src/client/service/buy_subscription.py
@@ -11,3 +11,28 @@ def main(user_info):
         # payment stuff and changes in db (should use othere modules and db)
         
         # returns user_info
+
+
+
+    #===========================Please Check my Code==========================
+
+    while True:
+        print('please choose your package:\n  1. silver\n  2. gold')
+
+        user_choice = input("Enter package number (1/2) or 'q' to quit: ")
+        if user_choice == 'q':
+            print('Exiting the program...')
+            break
+        elif user_choice == '1':
+            user_package = 'silver'
+        elif user_choice == '2':
+            user_package = 'gold'
+        else:
+            user_package = 'bronze'
+
+        if user_choice in {'1', '2'}:
+            print("You selected", user_package)
+            print('At this step the user should be taken to Payment stuff and changes in db')
+            break
+        else:
+            print("Invalid package number. Please try again.")
